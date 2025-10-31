dd02cec9d3d995cdb8891830bc2a4fb2b6c739a5
vince.h <v.harmegnies@gmail.com>
2025-10-31 11:02:21 +0100

    exo75-profil(moche)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {FormationCatalogComponent} from './views/formation/formation-catalog/for
 import {FormationCreationComponent} from './views/formation/formation-creation/formation-creation.component';
 import {FormationDetailComponent} from './views/formation/formation-detail/formation-detail.component';
 import {HomeComponent} from './views/home/home.component';
+import {UserProfileComponent} from './views/components/user-profile/user-profile.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -10,4 +11,6 @@ export const routes: Routes = [
   {path: 'catalog', component: FormationCatalogComponent},
   {path: 'create', component: FormationCreationComponent},
   {path: 'formation/:id', component: FormationDetailComponent},
+  {path: 'profil', component: UserProfileComponent },
+
 ];
